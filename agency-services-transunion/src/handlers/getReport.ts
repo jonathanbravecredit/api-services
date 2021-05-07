d0d7b4fcf18272453051eaccd2c42a9eb3842258
jonathanbravecredit <jonathan@brave.credit>
2021-05-07 15:47:02 -0700

    removing subnet 3

--- agency-services-transunion/src/handlers/getReport.ts
+++ agency-services-transunion/src/handlers/getReport.ts
@@ -1,17 +1,24 @@
-import { Handler, SNSEvent } from 'aws-lambda';
+import { SQSEvent, SQSHandler } from 'aws-lambda';
 import { response } from 'lib/utils/response';
 import axios from 'axios';
 
-export const main: Handler = async (event: SNSEvent): Promise<any> => {
+export const main: SQSHandler = async (event: SQSEvent): Promise<any> => {
   console.log('I received message --->', event);
   const dte = new Date();
   console.log(`I was called at: ${dte.toLocaleString()}`);
   // return response(200, 'Speaking to you from the VPC');
 
   try {
-    const res = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
-    console.log('my response from the external api', res);
-    return response(200, { response: res });
+    for (const record of event.Records) {
+      console.log('record=', record);
+      // const messageAttributes: SQSMessageAttributes = record.messageAttributes;
+      // console.log('Message Attributtes -->  ', messageAttributes.AttributeNameHere.stringValue);
+      // console.log('Message Body -->  ', record.body);
+      // // Do something
+      let res = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
+      console.log('my response from the external api', res);
+    }
+    return response(200, { response: 'sucessfully processed all messages' });
   } catch (err) {
     return response(500, { error: err });
   }
