8247aa4372e90d654751562222b219d7bcbbbac3
jonathanbravecredit <jonathan@brave.credit>
2021-05-07 15:36:35 -0700

    adding call to external api

--- agency-services-transunion/src/handlers/getReport.ts
+++ agency-services-transunion/src/handlers/getReport.ts
@@ -1,9 +1,18 @@
 import { Handler, SNSEvent } from 'aws-lambda';
 import { response } from 'lib/utils/response';
+import axios from 'axios';
 
 export const main: Handler = async (event: SNSEvent): Promise<any> => {
   console.log('I received message --->', event);
   const dte = new Date();
   console.log(`I was called at: ${dte.toLocaleString()}`);
   // return response(200, 'Speaking to you from the VPC');
+
+  try {
+    const res = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
+    console.log('my response from the external api', res);
+    return response(200, { response: res });
+  } catch (err) {
+    return response(500, { error: err });
+  }
 };
