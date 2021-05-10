18ce8a68e9ddc09d2b2b575579650e03c497d00b
jonathanbravecredit <jonathan@brave.credit>
2021-05-10 11:37:11 -0700

    adding test certs

--- agency-services-transunion/src/handlers/getReport.ts
+++ agency-services-transunion/src/handlers/getReport.ts
@@ -1,6 +1,10 @@
 import { SQSEvent, SQSHandler } from 'aws-lambda';
 import { response } from 'lib/utils/response';
 import axios from 'axios';
+import * as https from 'https';
+import * as fs from 'fs';
+
+let caCert: Buffer;
 
 export const main: SQSHandler = async (event: SQSEvent): Promise<any> => {
   console.log('I received message --->', event);
@@ -9,13 +13,24 @@ export const main: SQSHandler = async (event: SQSEvent): Promise<any> => {
   // return response(200, 'Speaking to you from the VPC');
 
   try {
+    caCert = fs.readFileSync('lib/data/brave.credit.crt');
+  } catch (err) {
+    console.log('Make sure that the CA cert file is named brave.credit.crt', err);
+    return response(500, { error: `Error gathering reading cert=${err}` });
+  }
+
+  const httpsAgent = new https.Agent({ ca: caCert, keepAlive: false });
+
+  try {
     for (const record of event.Records) {
       console.log('record=', record);
       // const messageAttributes: SQSMessageAttributes = record.messageAttributes;
       // console.log('Message Attributtes -->  ', messageAttributes.AttributeNameHere.stringValue);
       // console.log('Message Body -->  ', record.body);
       // // Do something
-      let res = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
+      let res = await axios.get('https://jsonplaceholder.typicode.com/todos/1', {
+        httpsAgent: httpsAgent,
+      });
       console.log('my response from the external api', res);
     }
     return response(200, { response: 'sucessfully processed all messages' });
