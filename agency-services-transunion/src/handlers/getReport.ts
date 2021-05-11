94847dd6b656683739ea94194188306eca560a96
jonathanbravecredit <jonathan@brave.credit>
2021-05-11 13:35:33 -0700

    trying the sns approach

--- agency-services-transunion/src/handlers/getReport.ts
+++ agency-services-transunion/src/handlers/getReport.ts
@@ -22,42 +22,38 @@ export const main: SQSHandler = async (event: SQSEvent): Promise<any> => {
   // url = 'https://www.dataaccess.com/webservicesserver/NumberConversion.wso?wsdl';
   // let client = yield soap["createClientAsync"](url);
   // console.log(client)
-
   //   return response(200, {
   //     response: 'sucessfully processed all messages'
   //   });
-
-  try {
-    // CONFIRMED IMPORTED CORRECTLY
-    key = fs.readFileSync('/opt/tubravecredit.key');
-    cert = fs.readFileSync('/opt/brave.credit.crt');
-    cacert = fs.readFileSync('/opt/Root-CA-Bundle.crt');
-  } catch (err) {
-    return response(500, { error: `Error gathering reading cert=${err}` });
-  }
-
-  try {
-    let client = await soap.createClientAsync(url, {
-      wsdl_options: {
-        key,
-        cert,
-        user,
-        passphrase,
-        timeout: 5000,
-      },
-      wsdl_headers: {
-        Authorization: auth,
-      },
-    });
-    console.log('last request', client.lastRequest);
-    console.log('client ====> ', client);
-
-    for (const record of event.Records) {
-      // do something
-    }
-    return response(200, { response: 'sucessfully processed all messages' });
-  } catch (err) {
-    console.log('err ===> ', err);
-    return response(500, { error: err });
-  }
+  // try {
+  //   // CONFIRMED IMPORTED CORRECTLY
+  //   key = fs.readFileSync('/opt/tubravecredit.key');
+  //   cert = fs.readFileSync('/opt/brave.credit.crt');
+  //   cacert = fs.readFileSync('/opt/Root-CA-Bundle.crt');
+  // } catch (err) {
+  //   return response(500, { error: `Error gathering reading cert=${err}` });
+  // }
+  // try {
+  //   let client = await soap.createClientAsync(url, {
+  //     wsdl_options: {
+  //       key,
+  //       cert,
+  //       user,
+  //       passphrase,
+  //       timeout: 5000,
+  //     },
+  //     wsdl_headers: {
+  //       Authorization: auth,
+  //     },
+  //   });
+  //   console.log('last request', client.lastRequest);
+  //   console.log('client ====> ', client);
+  //   for (const record of event.Records) {
+  //     // do something
+  //   }
+  //   return response(200, { response: 'sucessfully processed all messages' });
+  // } catch (err) {
+  //   console.log('err ===> ', err);
+  //   return response(500, { error: err });
+  // }
 };
