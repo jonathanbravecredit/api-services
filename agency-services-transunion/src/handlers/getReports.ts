197ce3630f7b17fac31778862e4e2afeed42c93a
jonathanbravecredit <jonathan@brave.credit>
2021-05-11 17:17:52 -0700

    clean up

--- agency-services-transunion/src/handlers/getReports.ts
+++ agency-services-transunion/src/handlers/getReports.ts
@@ -1,10 +1,12 @@
-import { SQSEvent, SQSHandler } from 'aws-lambda';
+import { SNSEvent, SNSHandler, SQSEvent, SQSHandler } from 'aws-lambda';
 import { response } from 'lib/utils/response';
 import * as fs from 'fs';
+import * as request from 'request';
 import * as soap from 'soap';
+import { ISecurity } from 'soap';
 import { getSecretKey } from 'lib/utils/secrets';
 
-// request.debug = true; import * as request from 'request';
+// request.debug = true;
 const transunionSKLoc = process.env.TU_SECRET_LOCATION;
 let key: Buffer;
 let cert: Buffer;
@@ -17,6 +19,8 @@ let passphrase;
 let password;
 
 export const main: SQSHandler = async (event: SQSEvent): Promise<any> => {
+  // batch up items through the day/evening to process lots of data
+
   try {
     const secretJSON = await getSecretKey(transunionSKLoc);
     const { tuKeyPassphrase, tuPassword } = JSON.parse(secretJSON);
@@ -26,32 +30,5 @@ export const main: SQSHandler = async (event: SQSEvent): Promise<any> => {
     auth = 'Basic ' + Buffer.from(user).toString('base64');
   } catch (err) {
     return response(500, { error: `Error gathering/reading secrets=${err}` });
-  }
-
-  try {
-    key = fs.readFileSync('/opt/tubravecredit.key');
-    cert = fs.readFileSync('/opt/brave.credit.crt');
-    cacert = fs.readFileSync('/opt/Root-CA-Bundle.crt');
-  } catch (err) {
-    return response(500, { error: `Error gathering/reading cert=${err}` });
-  }
-  try {
-    let client = await soap.createClientAsync(url, {
-      wsdl_options: {
-        key,
-        cert,
-        user,
-        passphrase,
-      },
-      wsdl_headers: {
-        Authorization: auth,
-      },
-    });
-    for (const record of event.Records) {
-      // do something
-    }
-    return response(200, { response: 'sucessfully processed all messages' });
-  } catch (err) {
-    return response(500, { error: err });
   }
 };
