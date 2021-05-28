6360f18c0e6b23ffdefd0fb9548c7b98bcebf481
jonathanbravecredit <jonathan@brave.credit>
2021-05-28 14:04:48 -0700

    turning off getReports

--- agency-services-transunion/src/handlers/getReport.ts
+++ agency-services-transunion/src/handlers/getReport.ts
@@ -26,41 +26,40 @@ let password;
  * @returns Lambda proxy response
  */
 export const main: SNSHandler = async (event: SNSEvent): Promise<any> => {
-  try {
-    const secretJSON = await getSecretKey(transunionSKLoc);
-    const { tuKeyPassphrase, tuPassword } = JSON.parse(secretJSON);
-    password = tuPassword;
-    passphrase = tuKeyPassphrase;
-    user = `${username}:${password}`;
-    auth = 'Basic ' + Buffer.from(user).toString('base64');
-  } catch (err) {
-    return response(500, { error: `Error gathering/reading secrets=${err}` });
-  }
-
-  try {
-    key = fs.readFileSync('/opt/tubravecredit.key');
-    cert = fs.readFileSync('/opt/brave.credit.crt');
-    cacert = fs.readFileSync('/opt/Root-CA-Bundle.crt');
-  } catch (err) {
-    return response(500, { error: `Error gathering/reading cert=${err}` });
-  }
-  try {
-    let client = await soap.createClientAsync(url, {
-      wsdl_options: {
-        key,
-        cert,
-        user,
-        passphrase,
-      },
-      wsdl_headers: {
-        Authorization: auth,
-      },
-    });
-    for (const record of event.Records) {
-      // do something
-    }
-    return response(200, { response: 'sucessfully processed all messages' });
-  } catch (err) {
-    return response(500, { error: err });
-  }
+  // try {
+  //   const secretJSON = await getSecretKey(transunionSKLoc);
+  //   const { tuKeyPassphrase, tuPassword } = JSON.parse(secretJSON);
+  //   password = tuPassword;
+  //   passphrase = tuKeyPassphrase;
+  //   user = `${username}:${password}`;
+  //   auth = 'Basic ' + Buffer.from(user).toString('base64');
+  // } catch (err) {
+  //   return response(500, { error: `Error gathering/reading secrets=${err}` });
+  // }
+  // try {
+  //   key = fs.readFileSync('/opt/tubravecredit.key');
+  //   cert = fs.readFileSync('/opt/brave.credit.crt');
+  //   cacert = fs.readFileSync('/opt/Root-CA-Bundle.crt');
+  // } catch (err) {
+  //   return response(500, { error: `Error gathering/reading cert=${err}` });
+  // }
+  // try {
+  //   let client = await soap.createClientAsync(url, {
+  //     wsdl_options: {
+  //       key,
+  //       cert,
+  //       user,
+  //       passphrase,
+  //     },
+  //     wsdl_headers: {
+  //       Authorization: auth,
+  //     },
+  //   });
+  //   for (const record of event.Records) {
+  //     // do something
+  //   }
+  //   return response(200, { response: 'sucessfully processed all messages' });
+  // } catch (err) {
+  //   return response(500, { error: err });
+  // }
 };
