4f78324c14c7b23131d9f8e05297096922b19273
jonathanbravecredit <jonathan@brave.credit>
2021-05-28 20:30:18 -0700

    save

--- agency-services-transunion/src/handlers/getReport.ts
+++ agency-services-transunion/src/handlers/getReport.ts
@@ -1,8 +1,6 @@
 import { SNSEvent, SNSHandler } from 'aws-lambda';
 import { response } from 'lib/utils/response';
-import * as fs from 'fs';
 import * as soap from 'soap';
-import { getSecretKey } from 'lib/utils/secrets';
 
 let url = 'http://www.thomas-bayer.com/axis2/services/BLZService?wsdl';
 
@@ -12,7 +10,10 @@ let url = 'http://www.thomas-bayer.com/axis2/services/BLZService?wsdl';
 export const main: SNSHandler = async (event: SNSEvent): Promise<any> => {
   try {
     let client = await soap.createClientAsync(url);
+    // const res = await wait(client.)
     console.log(client.describe());
+    const res = await wait(client);
+    console.log('res', res);
     for (const record of event.Records) {
       // do something
     }
@@ -20,4 +21,12 @@ export const main: SNSHandler = async (event: SNSEvent): Promise<any> => {
   } catch (err) {
     return response(500, { error: err });
   }
+};
+
+const wait = (client: soap.Client) => {
+  return new Promise((resolve, reject) => {
+    client.getBankAsync({}).then((result) => {
+      resolve(result);
+    });
+  });
 };
