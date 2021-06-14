8eb8dcd8f6880d1542529fe5a349196fc18b8117
jonathanbravecredit <jonathan@brave.credit>
2021-06-14 16:01:48 -0700

    save

--- agency-services-transunion/src/handlers/graphql.ts
+++ agency-services-transunion/src/handlers/graphql.ts
@@ -176,7 +176,7 @@ const proxyHandler = {
     const options = createRequestOptions(agent, auth, xml, 'Enroll');
     const res = await axios({ ...options });
     console.log('Response xml ====> ', JSON.stringify(res.data));
-    const results = await syncAndSaveEnroll2(parseEnroll(res.data));
+    const results = await syncAndSaveEnroll1(parseEnroll(res.data));
     console.log('results', results);
     return results;
   },
