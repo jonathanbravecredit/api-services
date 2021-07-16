eb6123fb22ce177e0e623324bf5fe7d5ffe0c5d0
jonathanbravecredit <jonathan@brave.credit>
2021-07-16 12:22:53 -0700

    removing ajv for now
    can't fix webpack error

--- agency-services-transunion/lib/schema/validation.ts
+++ agency-services-transunion/lib/schema/validation.ts
@@ -1,4 +1,4 @@
-import Ajv from 'ajv';
-import * as schemaGetAppDataRequest from './schema_get-app-data-request.json';
-export const ajv = new Ajv();
-ajv.addSchema(schemaGetAppDataRequest, 'getAppDataRequest');
+// import Ajv from 'ajv';
+// import * as schemaGetAppDataRequest from './schema_get-app-data-request.json';
+// export const ajv = new Ajv();
+// ajv.addSchema(schemaGetAppDataRequest, 'getAppDataRequest');
