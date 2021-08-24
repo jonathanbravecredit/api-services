908d8fa86e0bb310f70b9044c483baaba0bc677e
Jonathan <jonathan@brave.credit>
2021-08-23 20:04:36 -0700

    fixing async method

--- agency-services-transunion/lib/utils/tokens/tokens.ts
+++ agency-services-transunion/lib/utils/tokens/tokens.ts
@@ -10,25 +10,15 @@ export const validateToken = async (token: string | undefined): Promise<string |
     if (token) {
       const decodedToken = jwt.decode(token, { complete: true });
       console.log('decoded token ===> ', decodedToken);
-      //https://cognito-idp.us-east-2.amazonaws.com/us-east-2_beTcykTM9/.well-known/jwks.json
-      await axios({
+      const { data } = await axios({
         method: 'GET',
         url: `https://cognito-idp.${process.env.AWS_REGION}.amazonaws.com/${process.env.POOL_ID}/.well-known/jwks.json`,
-      }).then((resp) => {
-        if (resp['statusCode'] != 200) {
-          throw new Error(
-            `Request of JWT key with unexpected statusCode: expecting 200, received ${resp['statusCode']}`,
-          );
-        }
-        const { data } = resp; //GET THE REPSONCE BODY
-        const jwks: IJwks = JSON.parse(data); //body is a string, convert it to JSON
-        console.log('parsed jwks ===> ', jwks);
-        // body is an array of more than one JW keys.  User the key id in the JWT header to select the correct key object
-        const keyObject = jwks.keys.find((key) => key.kid == decodedToken.header.kid);
-        pem = jwkToPem(keyObject); //convert jwk to pem
       });
+      const jwks: IJwks = JSON.parse(data);
+      console.log('parsed jwks ===> ', jwks);
+      const keyObject = jwks.keys.find((key) => key.kid == decodedToken.header.kid);
+      pem = jwkToPem(keyObject); //convert jwk to pem
     }
-    //VERIFY THE JWT SIGNATURE. IF THE SIGNATURE IS VALID, THEN ADD THE JWT TO THE IDENTITY OBJECT.
     jwt.verify(token, pem, (error, decoded) => {
       if (error) {
         console.error(error);
