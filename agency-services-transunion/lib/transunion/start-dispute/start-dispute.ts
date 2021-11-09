a97e06483a49b236899ec759865d6261e0cd47ac
Jonathan <jonathan@brave.credit>
2021-11-08 16:07:00 -0800

    save

--- agency-services-transunion/lib/transunion/start-dispute/start-dispute.ts
+++ agency-services-transunion/lib/transunion/start-dispute/start-dispute.ts
@@ -705,9 +705,9 @@ export const createStartDisputePersonal = (msg: IStartDispute): string => {
             'data:Employers': mappedEmployers,
             'data:EnrollmentKey': textConstructor(msg.request.EnrollmentKey),
             'data:IndicativeDisputes': mappedIndicativeDisputes,
-            'data:LineItems': {
-              'data:LineItem': textConstructor(null, true),
-            },
+            // 'data:LineItems': {
+            //   'data:LineItem': textConstructor(null, true),
+            // },
             'data:ServiceBundleFulfillmentKey': textConstructor(msg.request.ServiceBundleFulfillmentKey),
             // 'data:ServiceProductFulfillmentKey': textConstructor(msg.request.ServiceProductFulfillmentKey, true),
           },
