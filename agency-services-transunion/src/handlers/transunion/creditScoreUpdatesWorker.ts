844a7125983333d4c261ed6a9ef3395c6e1a5d7f
Jonathan <jonathan@brave.credit>
2022-01-04 18:11:40 -0800

    save

--- agency-services-transunion/src/handlers/transunion/creditScoreUpdatesWorker.ts
+++ agency-services-transunion/src/handlers/transunion/creditScoreUpdatesWorker.ts
@@ -70,6 +70,7 @@ export const main: SQSHandler = async (event: SQSEvent): Promise<any> => {
       return JSON.parse(r.body) as ITransunionBatchPayload<IGetEnrollmentData>;
     });
     console.log(`Received ${records.length} records `);
+    console.log('records ===> ', JSON.stringify(records));
     const httpsAgent = new Agent({
       key,
       cert,
