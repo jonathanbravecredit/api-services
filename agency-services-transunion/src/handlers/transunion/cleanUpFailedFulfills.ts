fa0c4a54892ab826cef2a1995f034f45131b05d6
Jonathan <jonathan@brave.credit>
2022-01-25 10:48:58 -0800

    adding clean up for failed fulfills

---
+++ agency-services-transunion/src/handlers/transunion/cleanUpFailedFulfills.ts
@@ -0,0 +1,41 @@
+import { Handler } from 'aws-lambda';
+import { listTransactionLog } from 'lib/utils/db/logger/queries/api-transaction.queries';
+import { Sync } from 'lib/utils/sync/sync';
+import * as tu from 'lib/transunion';
+import { IFulfillResult } from 'lib/interfaces';
+
+export const main: Handler = async (event: any): Promise<void> => {
+  const { list } = event as { list: string[] }; // list of failed fulfill IDs
+  if (!list || !list.length) return;
+  // get the FulfillWorker:data action from transaction logs
+  try {
+    await Promise.all(
+      list.map(async (id) => {
+        const actions = (await listTransactionLog(id))
+          .filter((log) => log.action === 'FulfillWorker:data')
+          .sort((a, b) => {
+            return new Date(b.createdOn).valueOf() - new Date(a.createdOn).valueOf();
+          })[0];
+        if (!actions) return;
+        const data = JSON.parse(actions.transaction) as IFulfillResult;
+        const timestamp = '2022-01-19T08:00:00.000Z';
+        // update the data base with the parsed results;
+        const sync = new Sync(tu.enrichFulfillData);
+        // get the specific response from parsed object
+        const responseType = data?.ResponseType;
+        const error = data?.ErrorResponse;
+
+        let response;
+        if (responseType.toLowerCase() === 'success') {
+          const synced = await sync.syncData({ id }, data, false);
+          response = synced
+            ? { success: true, error: null, data: data }
+            : { success: false, error: 'failed to sync data to db' };
+        } else {
+          response = { success: false, error: error };
+        }
+      }),
+    );
+  } catch (err) {}
+  return;
+};
