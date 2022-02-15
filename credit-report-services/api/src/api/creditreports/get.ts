15fadda7a357bf7d09a5157aa85be2b4658791cb
Jonathan <jonathan@brave.credit>
2022-02-14 17:40:06 -0800

    adding test script

--- credit-report-services/api/src/api/creditreports/get.ts
+++ credit-report-services/api/src/api/creditreports/get.ts
@@ -1,9 +1,14 @@
 import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda';
 import { response } from 'libs/utils/response';
+import * as json from 'libs/data/example.json';
+import { MergeReport } from 'libs/models/MergeReport/MergeReport';
+import { IMergeReport } from 'libs/interfaces/merge-report.interface';
 
 export const main: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  console.log('event: ', JSON.stringify(event));
-  const sub = event?.requestContext?.authorizer?.claims?.sub;
-  if (!sub) return response(200, null);
+  const tuRaw = json as unknown as IMergeReport;
+  const report = new MergeReport(tuRaw);
+  console.log('report 1: ', JSON.stringify(report));
+  console.log('spacer==========================>');
+  console.log('report 2: ', report);
   return response(200, null);
 };
