dc004456263d69ac8068b58cf1924e822181c4a6
Jonathan <jonathan@brave.credit>
2022-02-15 16:01:47 -0800

    adding additional logging

--- credit-report-services/api/src/subs/creditreports/create.ts
+++ credit-report-services/api/src/subs/creditreports/create.ts
@@ -8,7 +8,7 @@ export const main: SQSHandler = async (event: SQSEvent): Promise<void> => {
   const reportRequests = event.Records.map((r) => {
     return JSON.parse(r.body) as IBatchPayload<ICreditReportPayload>;
   });
-  console.log('report requests: ', reportRequests);
+  console.log('report requests: ', JSON.stringify(reportRequests));
   if (reportRequests.length) {
     try {
       Promise.all(
@@ -18,11 +18,13 @@ export const main: SQSHandler = async (event: SQSEvent): Promise<void> => {
           const added = new CreditReportMaker(userId, bureau, report);
           const current = new CreditReportMaker(userId, bureau, report, 0); // version 0 is always current
           current.setCurrentVersion(added.version);
-          console.log('added: ', added);
+          console.log('added: ', JSON.stringify(added));
           console.log('current: ', current);
           try {
-            await createReport(added);
-            await updateReport(current);
+            const create = await createReport(added);
+            console.log('create', create);
+            const update = await updateReport(current);
+            console.log('update', update);
           } catch (err) {
             console.log('err: ', err);
           }
