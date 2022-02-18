daf5cfe26d537831e05cee831258b64b4e0bd32d
Jonathan <jonathan@brave.credit>
2022-02-17 20:38:22 -0800

    adding the publisher
    now publishing the report to the credit report service

---
+++ agency-services-transunion/lib/transunion/credit-report-service/CreditReportPublisher.ts
@@ -0,0 +1,23 @@
+import { SNS } from 'aws-sdk';
+import { ICreditReportPayload } from 'lib/interfaces/batch.interfaces';
+import { PubSubUtil } from 'lib/utils/pubsub/pubsub';
+import { BraveParsers } from 'lib/utils/brave/parser/BraveParser';
+import { IMergeReport } from 'lib/interfaces/merge-report.interface';
+
+export class CreditReportPublisher {
+  constructor(private spo: string) {}
+
+  async publish(id: string): Promise<void> {
+    const report = BraveParsers.parseTransunionMergeReport(this.spo) as unknown as IMergeReport; // make sure the right merge report
+    const pubsub = new PubSubUtil();
+    const topic = process.env.CREDITREPORTS_SNS_PROXY_ARN;
+    const payload = {
+      userId: id,
+      bureau: 'transunion',
+      report: report,
+    } as ICreditReportPayload;
+    const snsPayload = pubsub.createSNSPayload<ICreditReportPayload>('create', payload, 'creditreports', topic);
+    const sns = new SNS({ region: 'us-east-2' });
+    await sns.publish(snsPayload).promise();
+  }
+}
