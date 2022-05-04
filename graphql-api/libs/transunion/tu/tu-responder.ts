ab006ba72a48a4c5a3dcbde0133ca09d30af4899
Jonathan <jonathan@brave.credit>
2022-05-03 21:09:31 -0700

    adding back logs

--- graphql-api/libs/transunion/tu/tu-responder.ts
+++ graphql-api/libs/transunion/tu/tu-responder.ts
@@ -15,7 +15,7 @@ export class TUResponder<RES, U> {
     if (!this.xml) throw 'No XML set';
     const obj: RES = fastXml.parse(this.xml, options);
     this.response = obj;
-    // console.log('response: ', JSON.stringify(this.response));
+    console.log('response: ', JSON.stringify(this.response));
     return this.response;
   }
 
