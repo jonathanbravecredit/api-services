9f920f5ae440876388d1fe350333913dc47135ec
Jonathan <jonathan@brave.credit>
2022-05-07 15:19:02 -0700

    removing additional property from payload prep

---
+++ graphql-api/libs/data/test.ts
@@ -0,0 +1,13 @@
+const verifyAuthTest = {
+  answers: [
+    {
+      VerifyChallengeAnswersRequestMultiChoiceQuestion: {
+        QuestionId: 60365668,
+        SelectedAnswerChoice: {
+          AnswerChoiceId: 3134810152,
+        },
+      },
+    },
+  ],
+  key: '0eb185e0-d9ec-4fa7-8fb3-a5b10c87084b',
+};
