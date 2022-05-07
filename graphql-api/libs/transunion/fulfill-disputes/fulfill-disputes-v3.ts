ba9b75f4a4915b5f27b6f1c89133ea02279634c7
Jonathan <jonathan@brave.credit>
2022-05-07 14:29:30 -0700

    fixing incorrect bundle code

--- graphql-api/libs/transunion/fulfill-disputes/fulfill-disputes-v3.ts
+++ graphql-api/libs/transunion/fulfill-disputes/fulfill-disputes-v3.ts
@@ -3,7 +3,7 @@ import { FulfillV3 } from 'libs/transunion/fulfill/fulfill-v3';
 
 export class FulfillDisputesV3 extends FulfillV3 {
   public action = 'FulfillDisputes';
-  public serviceBundleCode = 'CC2BraveCreditTUReportV3Score';
+  public serviceBundleCode = 'CC2BraveCreditTUReport24Hour';
   constructor(protected payload: IProxyRequest) {
     super(payload, 'FulfillDisputes');
   }
